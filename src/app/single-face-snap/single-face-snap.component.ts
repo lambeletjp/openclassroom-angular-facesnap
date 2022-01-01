5f4678df703090dd59ec24c6b4cd67838d5accb5
Jean-Pierre Lambelet <lambeletjp@gmail.com>
2022-01-01 22:04:26 +0100

    End of tutorial

---
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {FaceSnap} from "../models/face-snap.model";
+import {FaceSnapsService} from "../services/face-snaps.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-single-face-snap',
+  templateUrl: './single-face-snap.component.html',
+  styleUrls: ['./single-face-snap.component.scss']
+})
+export class SingleFaceSnapComponent implements OnInit {
+  faceSnap!: FaceSnap;
+  buttonText!: string;
+
+  constructor(private faceSnapsService: FaceSnapsService,
+              private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.buttonText = "Oh Snap!";
+    const faceSnapId = +this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapsService.getFaceSnapById(faceSnapId);
+  }
+
+  onSnap() {
+    if(this.buttonText === 'Oh Snap!') {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap')
+      this.buttonText = 'Oops, unSnap!'
+    }
+    else {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+      this.buttonText = 'Oh Snap!';
+    }
+  }
+
+}
